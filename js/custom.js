fc2629a9f9dda05a561d497e87699352bc342440
Vivian <vivyan3gmail.com>
2022-10-28 23:25:04 +0300

    hover effect created with a white box appearing with project name inside

--- js/custom.js
+++ js/custom.js
@@ -1,18 +1,4 @@
 
-//   $(document.ready(function() {
-
-
-//   $(document.ready(function() {
-//     $("#design-text").css('display','none');
-
-//     $("#design.jpg").hover(function() {
-
-//        $("#design-text",this).fadeIn();
-
-//        }, function() { $('#design-text').fadeOut();
-//     });
-//  }))
-
 $(document).ready(function () {
   $("#design_pic").click(function () {
     $("#design_pic").hide(1200);
@@ -24,9 +10,7 @@ $(document).ready(function () {
     $("#design_pic").toggle(1200);
     $("#design_pic").show(1200);
   });
-});
 
-//$(document).ready(function(){
 $("#development_pic").click(function () {
   $("#development_pic").hide(1200);
   $("#development").toggle(1200);
@@ -37,10 +21,7 @@ $("#development").click(function () {
   $("#development_pic").toggle(1200);
   $("#development_pic").show(1200);
 });
-// });
 
-
-//$(document).ready(function(){
 $("#product_pic").click(function () {
   $("#product_pic").hide(1200);
   $("#product").toggle(1200);
@@ -51,38 +32,123 @@ $("#product").click(function () {
   $("#product_pic").toggle(1200);
   $("#product_pic").show(1200);
 });
-//});
+$("#work1").hover(
+  function(){
+    $("#project1").show()
+    $(this).css(
+  { cursor:"pointer",opacity:"0.71"   }
+    )
+
+  },
+  function(){
+    $("#project1").hide()
+    $(this).css({opacity:"1"})
+  }
+)
+
+$("#work2").hover(
+  function(){
+    $("#project2").show()
+    $(this).css(
+      { cursor:"pointer",opacity:"0.71"   }
+        )
+
+  },
+  function(){
+    $("#project2").hide()
+    $(this).css({opacity:"1"})
+  }
+)
+
+$("#work3").hover(
+  function(){
+    $("#project3").show()
+    $(this).css(
+      { cursor:"pointer",opacity:"0.71"   }
+        )
+
+  },
+  function(){
+    $("#project3").hide()
+    $(this).css({opacity:"1"})
+  }
+)
+$("#work4").hover(
+  function(){
+    $("#project4").show()
+    $(this).css(
+      { cursor:"pointer",opacity:"0.71"   }
+        )
+
+  },
+  function(){
+    $("#project4").hide()
+    $(this).css({opacity:"1"})
+  }
+)
+
+$("#work5").hover(
+  function(){
+    $("#project5").show()
+    $(this).css(
+      { cursor:"pointer",opacity:"0.71"   }
+        )
+
+  },
+  function(){
+    $("#project5").hide()
+   
+    $(this).css({opacity:"1"})
+        
+  }
+)
+
+$("#work6").hover(
+  function(){
+    $("#project6").show()
+
+ $(this).css(
+  { cursor:"pointer",opacity:"0.71"   } )
+                                            
+  },
+  function(){
+    $("#project6").hide()
+    $(this).css({opacity:"1"})
+  }
+)
 
 
-//   $(document).ready(function () {
-//     $('#portfolios').hover(function () {
-//         $(this).animate({ opacity: '1' });
-//     },
-//         function () {
-//             $(this).animate({ opacity: '0' });
-//         })
-// });
+$("#work7").hover(
+  function(){
+    $("#project7").show()
+    $(this).css(
+      { cursor:"pointer",opacity:"0.71"   }
+        )
+
+  },
+  function(){
+    $("#project7").hide()
+    $(this).css({opacity:"1"})
+  }
+)
+
+$("#work8").hover(
+  function(){
+    $("#project8").show()
+    $(this).css(
+      { cursor:"pointer",opacity:"0.71"   }
+        )
+
+  },
+  function(){
+    $("#project8").hide()
+    $(this).css({opacity:"1"})
+  }
+)
 
 
-// $(document).ready(function () {
-//   $("#work1").hover(function () {
-//     // $(this).css("background-color", "white");
-//     // }, function(){
-//     // $(this).css("background-color", "blank");
-//     $(this).text("black");
-//   },
-//     function () {
-//       $(this).last().remove();
-
-//       $(this).show()
-//     }
-
-  //   );
-  // });
-       
 
 
 
-
-// });
+});
 
