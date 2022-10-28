0b43208ed331d75aef38e5291b11a18a2967d9e8
Vivian <vivyan3gmail.com>
2022-10-28 11:07:01 +0300

    added text area

--- js/custom.js
+++ js/custom.js
@@ -1,4 +1,20 @@
-$("design-text").click (function(){
-alert
+// $( "design" ).hover(
+//     function() {
+//       $( this ).addClass( "hover" );
+//     }, function() {
+//       $( this ).removeClass( "hover" );
+//     }
+//   );
 
-})
+
+  $(document.ready(function() {
+    $("#design-text").css('display','none');
+    
+    $("#design.jpg").hover(function() {
+       
+       $("#design-text",this).fadeIn();
+             
+       }, function() { $('#design-text').fadeOut();
+    });
+ }))
+ 
