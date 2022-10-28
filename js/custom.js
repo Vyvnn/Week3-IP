b4f220c03368396341b165be95aa619671087d4e
Vivian <vivyan3gmail.com>
2022-10-28 15:42:00 +0300

    'What we do' icons addedtoggle and hide effect when clicked

--- js/custom.js
+++ js/custom.js
@@ -4,12 +4,62 @@
 
 //   $(document.ready(function() {
 //     $("#design-text").css('display','none');
-    
+
 //     $("#design.jpg").hover(function() {
-       
+
 //        $("#design-text",this).fadeIn();
-             
+
 //        }, function() { $('#design-text').fadeOut();
 //     });
 //  }))
- 
+
+$(document).ready(function(){
+    $("#design_pic").click(function(){
+      $("#design_pic").hide(1200);
+      $("#design").toggle(1200);
+      $("#design").show(1200);
+    })
+    $("#design").click(function(){
+      $("#design").hide(1200);
+      $("#design_pic").toggle(1200);
+      $("#design_pic").show(1200);
+    });
+  });
+
+  $(document).ready(function(){
+    $("#development_pic").click(function(){
+      $("#development_pic").hide(1200);
+      $("#development").toggle(1200);
+      $("#development").show(1200);
+    })
+    $("#development").click(function(){
+      $("#development").hide(1200);
+      $("#development_pic").toggle(1200);
+      $("#development_pic").show(1200);
+    });
+  });
+
+
+  $(document).ready(function(){
+    $("#product_pic").click(function(){
+      $("#product_pic").hide(1200);
+      $("#product").toggle(1200);
+      $("#product").show(1200);
+    })
+    $("#product").click(function(){
+      $("#product").hide(1200);
+      $("#product_pic").toggle(1200);
+      $("#product_pic").show(1200);
+    });
+  });
+
+
+  $(document).ready(function () {
+    $('#portfolios').hover(function () {
+        $(this).animate({ opacity: '1' });
+    },
+        function () {
+            $(this).animate({ opacity: '0' });
+        })
+});
+
